a66462217e230d52aa35aa8ca6fdc03967f5b5ac
Songho Lee <sl222xk@student.lnu.se>
2017-12-24 01:38:28 +0900

    I am not sure if description of the implementation part is correct 😅

--- assignment3/dump/boat_club.sql
+++ assignment3/dump/boat_club.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: 127.0.0.1    Database: boat_club
+-- Host: www.songho.se    Database: boat_club
 -- ------------------------------------------------------
--- Server version	5.7.20
+-- Server version	5.7.20-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,8 +34,18 @@ CREATE TABLE `boat` (
   KEY `FK_MEMBER_ID_idx` (`member_id`),
   CONSTRAINT `FK_MEMBER_ID` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_TYPE_ID` FOREIGN KEY (`type_id`) REFERENCES `boat_type` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boat`
+--
+
+LOCK TABLES `boat` WRITE;
+/*!40000 ALTER TABLE `boat` DISABLE KEYS */;
+INSERT INTO `boat` VALUES (1,2015,20.4,2,2),(2,2010,20.4,3,1),(3,2008,21,4,6),(4,2000,20.6,3,4),(5,1940,30,5,6),(6,1996,50,7,3),(7,2002,20.6,6,5),(8,2009,20,3,2),(9,2002,23.7,2,4);
+/*!40000 ALTER TABLE `boat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `boat_type`
@@ -50,8 +60,18 @@ CREATE TABLE `boat_type` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `type_UNIQUE` (`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boat_type`
+--
+
+LOCK TABLES `boat_type` WRITE;
+/*!40000 ALTER TABLE `boat_type` DISABLE KEYS */;
+INSERT INTO `boat_type` VALUES (1,'Air boat'),(2,'Banana boat'),(3,'Ferry'),(4,'Fishing boat'),(5,'Kayak'),(6,'Sailboat');
+/*!40000 ALTER TABLE `boat_type` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `gender`
@@ -62,12 +82,22 @@ DROP TABLE IF EXISTS `gender`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gender` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(10) NOT NULL,
+  `type` varchar(15) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `type_UNIQUE` (`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gender`
+--
+
+LOCK TABLES `gender` WRITE;
+/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
+INSERT INTO `gender` VALUES (8,'Cis Man'),(10,'Cis Woman'),(6,'Male'),(12,'Not specified'),(9,'Trans Man'),(11,'Trans Woman'),(7,'Woman');
+/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `member`
@@ -83,9 +113,20 @@ CREATE TABLE `member` (
   `gender` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  CONSTRAINT `FK_GENDER_ID` FOREIGN KEY (`id`) REFERENCES `gender` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `FK_GENDER_ID_idx` (`gender`),
+  CONSTRAINT `FK_GENDER_ID` FOREIGN KEY (`gender`) REFERENCES `gender` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (2,'Songho Lee',22,6),(3,'Sarpreet Singh Buttar',22,6),(4,'Lars',50,6),(5,'Max Mueller',30,8),(6,'Anita Minta',27,10),(7,'Emma Svensson',80,7),(8,'Saebom Park',26,10);
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -96,4 +137,4 @@ CREATE TABLE `member` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-16 13:01:14
+-- Dump completed on 2017-12-24  1:27:26
